f09562ad4471e6f7d91018a7ea951dfce870e639
maanav <shah.maanav.07@gmail.com>
2019-12-23 23:47:32 +0530

    Add splasher animation activity

--- app/src/main/java/com/maanavshah/makemoney/MainActivity.java
+++ app/src/main/java/com/maanavshah/makemoney/MainActivity.java
@@ -2,22 +2,38 @@ package com.maanavshah.makemoney;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.mrgames13.jimdo.splashscreen.App.SplashScreenBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        setTheme(R.style.AppTheme);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(this, LoginActivity.class));
-        finish();
+
+        SplashScreenBuilder.Companion.getInstance(this)
+                .setVideo(R.raw.splash_animation)
+                .setImage(R.drawable.app_icon)
+                .show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SplashScreenBuilder.Companion.getSPLASH_SCREEN_FINISHED()) {
+            if (resultCode == RESULT_OK) {
+                // SplashScreen finished without manual canceling
+
+                startActivity(new Intent(this, LoginActivity.class));
+                finish();
+            } else if (resultCode == RESULT_CANCELED) {
+                // SplashScreen finished through manual canceling
+            }
+        }
     }
 
     @Override
