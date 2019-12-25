87092b6ddacaa1486d2a43e9af1960721519a929
maanav <shah.maanav.07@gmail.com>
2019-12-25 20:09:05 +0530

    Add watch video activity

--- app/src/main/java/com/maanavshah/makemoney/WatchVideoActivity.java
+++ app/src/main/java/com/maanavshah/makemoney/WatchVideoActivity.java
@@ -1,12 +1,16 @@
 package com.maanavshah.makemoney;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.daimajia.numberprogressbar.NumberProgressBar;
+import com.daimajia.numberprogressbar.OnProgressBarListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
@@ -14,22 +18,24 @@ import com.google.android.gms.ads.initialization.OnInitializationCompleteListene
 import com.google.android.gms.ads.reward.RewardItem;
 import com.google.android.gms.ads.reward.RewardedVideoAd;
 import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+import com.maanavshah.makemoney.Helper.SharedConfig;
 
-public class WatchVideoActivity extends AppCompatActivity implements RewardedVideoAdListener {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class WatchVideoActivity extends AppCompatActivity implements RewardedVideoAdListener, OnProgressBarListener {
 
     private static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/5224354917";
-
     private RewardedVideoAd rewardedVideoAd;
-    private Button retryButton;
     private Button showVideoButton;
+    private NumberProgressBar bnp;
+    private TextView tv_retry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_video);
 
-        // Initialize the Mobile Ads SDK.
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
@@ -38,18 +44,27 @@ public class WatchVideoActivity extends AppCompatActivity implements RewardedVid
 
         rewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
         rewardedVideoAd.setRewardedVideoAdListener(this);
+        tv_retry = findViewById(R.id.tv_retry);
 
         loadRewardedVideoAd();
-        Toast.makeText(this, "Loading Reward Video", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Loading Reward Video!", Toast.LENGTH_SHORT).show();
 
-        retryButton = findViewById(R.id.retry_button);
-        retryButton.setVisibility(View.INVISIBLE);
-        retryButton.setOnClickListener(new View.OnClickListener() {
+        bnp = findViewById(R.id.number_progress_bar);
+        bnp.setOnProgressBarListener(this);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Show another add", Toast.LENGTH_SHORT).show();
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (bnp.getProgress() < 99) {
+                            bnp.incrementProgressBy(1);
+                        }
+                    }
+                });
             }
-        });
+        }, 1000, 100);
 
         // Create the "show" button, which shows a rewarded video if one is loaded.
         showVideoButton = findViewById(R.id.show_video_button);
@@ -60,68 +75,84 @@ public class WatchVideoActivity extends AppCompatActivity implements RewardedVid
                 showRewardedVideo();
             }
         });
-
     }
 
     private void showRewardedVideo() {
         showVideoButton.setVisibility(View.INVISIBLE);
         if (rewardedVideoAd.isLoaded()) {
             rewardedVideoAd.show();
+        } else {
+            loadRewardedVideoAd();
+            Toast.makeText(getApplicationContext(), "Loading Ad!", Toast.LENGTH_SHORT).show();
         }
     }
-
 
     private void loadRewardedVideoAd() {
         if (!rewardedVideoAd.isLoaded()) {
             rewardedVideoAd.loadAd(AD_UNIT_ID, new AdRequest.Builder().build());
+            if (tv_retry != null) {
+                tv_retry.setVisibility(View.VISIBLE);
+            }
+            if (bnp != null) {
+                bnp.setVisibility(View.VISIBLE);
+            }
         }
     }
 
-
     @Override
     public void onRewardedVideoAdLoaded() {
-        Toast.makeText(this, "onRewardedVideoAdLoaded", Toast.LENGTH_SHORT).show();
+        bnp.setProgress(100);
         showVideoButton.setVisibility(View.VISIBLE);
+        tv_retry.setVisibility(View.INVISIBLE);
+        bnp.setVisibility(View.INVISIBLE);
     }
 
     @Override
     public void onRewardedVideoAdOpened() {
-        Toast.makeText(this, "onRewardedVideoAdOpened", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onRewardedVideoStarted() {
-        Toast.makeText(this, "onRewardedVideoStarted", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onRewardedVideoAdClosed() {
-        Toast.makeText(this, "onRewardedVideoAdClosed", Toast.LENGTH_SHORT).show();
-
         // Preload the next video ad.
         loadRewardedVideoAd();
     }
 
     @Override
     public void onRewarded(RewardItem reward) {
-        Toast.makeText(this,
-                String.format(" onRewarded! currency: %s amount: %d", reward.getType(),
-                        reward.getAmount()),
-                Toast.LENGTH_SHORT).show();
+        String add_coins = SharedConfig.getConfig(getApplicationContext(), "add_coins");
+        String email = SharedConfig.getConfig(getApplicationContext(), "email");
+        String coins = SharedConfig.getConfig(getApplicationContext(), email);
+        SharedConfig.setConfig(getApplicationContext(), email, String.valueOf(Integer.valueOf(coins) + Integer.valueOf(add_coins)));
+        bnp.setProgress(0);
     }
 
     @Override
     public void onRewardedVideoAdLeftApplication() {
-        Toast.makeText(this, "onRewardedVideoAdLeftApplication", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onRewardedVideoAdFailedToLoad(int i) {
-        Toast.makeText(this, "onRewardedVideoAdFailedToLoad", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onRewardedVideoCompleted() {
-        Toast.makeText(this, "onRewardedVideoCompleted", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProgressChange(int current, int max) {
+        if (current == max) {
+            bnp.setProgress(0);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(getApplicationContext(), NavigationActivity.class));
+        finish();
+        super.onBackPressed();
     }
 }
